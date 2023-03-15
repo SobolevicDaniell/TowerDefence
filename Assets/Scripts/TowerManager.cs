17b8df3abd4365f7b66860d8eeafc7820466abbc
SobolevicDaniell <sobolevdan99@yandex.ru>
2023-03-15 12:57:05 +0300

    Add TowerPlacement

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.EventSystems;
+using UnityEngine.EventSystems;
 
 public class TowerManager : Loader<TowerManager>
 {
@@ -12,7 +14,27 @@ public class TowerManager : Loader<TowerManager>
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.zero);
+
+            if (hit.collider.tag == "TowerSide")
+            {
+                hit.collider.tag = "TowerSideFull";
+                
+                PlaceTower(hit);
+            }
+        }
+    }
+
+    public void PlaceTower(RaycastHit2D hit)
+    {
+        if (!EventSystem.current.IsPointerOverGameObject() && _towerBtnPressed != null)
+        {
+            GameObject newTower = Instantiate(_towerBtnPressed.Tower);
+            newTower.transform.position = hit.transform.position;
+        }
     }
 
     public void SelectedTower(TowerBtn towerSelected)
